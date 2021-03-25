5dfece1a4145209b8cd9f43d1815817e86d5c880
Yejin <60729498+ProgramNami@users.noreply.github.com>
2021-03-25 12:58:49 +0900

    3월 25일 11053 가장 긴 증가하는  부분 수열

---
+++ src/boj/day0325/BOJ_11053_Main.java
@@ -0,0 +1,31 @@
+package boj.day0325;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11053_Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int ans = 0;
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int[] length = new int[n];
+		StringTokenizer str = new StringTokenizer(br.readLine());
+		for(int i= 0; i < n; i++) {
+			arr[i] = Integer.parseInt(str.nextToken());
+			length[i] = 1;
+		}
+		for(int i= 1; i < n; i++) {
+			for(int j = i-1; j > -1; j--) {
+				if(arr[i] > arr[j]) {
+					length[i] = Math.max(length[j] + 1, length[i]);
+				}
+			}
+		}
+		for(int i= 0; i < n; i++) {
+			ans = Math.max(ans, length[i]);
+		}
+		System.out.println(ans);
+	}
+}
